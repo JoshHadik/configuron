5a5ee7cffa02dcba5845cd2f05a0324072938fc7
Joshua Hadik <josh.hadik@gmail.com>
2018-04-22 20:51:25 -0500

    remove code cove

--- spec/configurable_spec.rb
+++ spec/configurable_spec.rb
@@ -1,12 +1,5 @@
 require_relative '../lib/configuron'
 
-require 'simplecov'
-SimpleCov.start
-
-require 'codecov'
-SimpleCov.formatter = SimpleCov::Formatter::Codecov
-
-
 RSpec.describe Configuron::Configurable do
   class Hello; end
   it "will raise an unconfigurable type error when added to anything other than a module" do
