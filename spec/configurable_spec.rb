05ce1b8e6179aadec00d23ae60c3c5b1cdcea94e
Joshua Hadik <josh.hadik@gmail.com>
2018-04-22 22:39:48 -0500

    switched tense of rspec it block

--- spec/configurable_spec.rb
+++ spec/configurable_spec.rb
@@ -3,7 +3,7 @@ require_relative '../lib/configuron'
 
 RSpec.describe Configuron::Configurable do
   class Hello; end
-  it "will raise an unconfigurable type error when added to anything other than a module" do
+  it "raises an unconfigurable type error when added to anything other than a module" do
     expect { Hello.extend(Configuron::Configurable) }.to raise_error(Configuron::Configurable::UnconfigurableTypeError)
   end
 end
