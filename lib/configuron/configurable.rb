a5386d1d082be05bdfd778f01b99279bd14a0d1a
Joshua Hadik <josh.hadik@gmail.com>
2018-04-24 11:43:33 -0500

    remove root method. assumes too much.

--- lib/configuron/configurable.rb
+++ lib/configuron/configurable.rb
@@ -1,34 +1,25 @@
-module Configuron
-  module Configurable
-    class UnconfigurableTypeError < StandardError; end
+module Configurable
+  class UnconfigurableTypeError < StandardError; end
 
-    def self.extended(mod)
-      extended_at_path, = caller[0].partition(":")
-      if mod.class == Module
-
-        add_configuration_methods_to(mod, extended_at_path)
-      else
-        raise UnconfigurableTypeError
-      end
+  def self.extended(mod)
+    if mod.is_a? Module
+      add_configuration_methods_to(mod)
+    else
+      raise UnconfigurableTypeError
     end
+  end
 
-    def self.add_configuration_methods_to(mod, extended_at_path)
-      eval_proc = Proc.new do |path|
-        @configuron_root_path = path
-        def root
-          Pathname.new(File.dirname @configuron_root_path)
-        end
-        def configuration
-          @configuration ||= self::Configuration.new
-        end
-        def reset!
-          @configuration = self::Configuration.new
-        end
-        def configure
-          yield(configuration)
-        end
+  def self.add_configuration_methods_to(mod)
+    mod.instance_eval do
+      def configuration
+        @configuration ||= self::Configuration.new
       end
-      mod.instance_exec(extended_at_path, &eval_proc)
+      def reset
+        @configuration = self::Configuration.new
+      end
+      def configure
+        yield(configuration)
+      end
     end
   end
 end
