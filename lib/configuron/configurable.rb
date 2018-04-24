8306411a255150ec92fc50ef3494b450a75ec980
Joshua Hadik <josh.hadik@gmail.com>
2018-04-24 11:44:33 -0500

    undid too much. undid undoes

--- lib/configuron/configurable.rb
+++ lib/configuron/configurable.rb
@@ -1,24 +1,26 @@
-module Configurable
-  class UnconfigurableTypeError < StandardError; end
+module Configuron
+  module Configurable
+    class UnconfigurableTypeError < StandardError; end
 
-  def self.extended(mod)
-    if mod.is_a? Module
-      add_configuration_methods_to(mod)
-    else
-      raise UnconfigurableTypeError
+    def self.extended(mod)
+      if mod.class == Module
+        add_configuration_methods_to(mod)
+      else
+        raise UnconfigurableTypeError
+      end
     end
-  end
 
-  def self.add_configuration_methods_to(mod)
-    mod.instance_eval do
-      def configuration
-        @configuration ||= self::Configuration.new
-      end
-      def reset
-        @configuration = self::Configuration.new
-      end
-      def configure
-        yield(configuration)
+    def self.add_configuration_methods_to(mod)
+      mod.instance_eval do
+        def configuration
+          @configuration ||= self::Configuration.new
+        end
+        def reset!
+          @configuration = self::Configuration.new
+        end
+        def configure
+          yield(configuration)
+        end
       end
     end
   end
